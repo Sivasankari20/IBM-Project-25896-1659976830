9ea172194ff952385734abf7d40608cd41a7a7ed
Sivasankari20 <112847857+Sivasankari20@users.noreply.github.com>
2022-10-31 14:34:43 +0530

    Delete primeseries.py

--- IBM project/Assessments/member 1(sivasankari)/Assignment 1/primeseries.py
+++
@@ -1,9 +0,0 @@
-a = 2
-b = int(input("Enter the max : "))
-for n in range(a,b+ 1):
-   if n > 1:
-       for i in range(2,n):
-           if (n % i) == 0:
-               break
-       else:
-           print(n)
